59d60181b3c85d7ffb3b9c77658f1ec14bbad262
Daniel1024 <tierra_santa_9@hotmail.com>
2016-02-12 20:27:12 -0500

    Subiendo segunda clase javascript todavia falta

--- tvify/js/index.js
+++ tvify/js/index.js
@@ -13,6 +13,44 @@ $(function () {
     //mostrar(header[0]);
     //$('#app-header').find('h1')
 
+    /**
+    *Submit search form
+    */
+    $('#app-body')
+        .find('form')
+        .submit(function(ev) {
+            ev.preventDefault();
+            var $busqueda = $(this)
+                .find('input[type="text"]')
+                .val();
+            alert('Se ha buscado: ' + $busqueda);
+    });
+
+    var template = '<article class="tv-show">' +
+                    '<div class="left">' +
+                        '<img src=":img:" alt=":imgalt:">' +
+                    '</div>' +
+                    '<div class="left info">' +
+                        '<h1>:name:</h1>' +
+                        '<p>:summary:</p>' +
+                    '</div>' +
+                '</article>';
+
+    $.ajax({
+        url: 'http://api.tvmaze.com/shows',
+        success: function (shows, textStatus, xhr) {
+            var $tvShowsContainer = $('.tv-shows');
+            shows.forEach(function (show) {
+                var article = template
+                    .replace(':name:', show.name)
+                    .replace(':img:', show.image.medium)
+                    .replace(':summary:', show.summary)
+                    .replace(':imgalt:', show.name + ' Logo');
+
+                $tvShowsContainer.append($(article));
+            })
+        }
+    });
 
 
 
