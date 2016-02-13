ce942f5c91cfdabaae336d68b6e06590d3451f0f
Daniel1024 <tierra_santa_9@hotmail.com>
2016-02-13 16:51:14 -0500

    Segunda clase javascript lista

--- tvify/js/index.js
+++ tvify/js/index.js
@@ -1,5 +1,31 @@
 $(function () {
+    function loadCSS(url) {
+        var elem = document.createElement('link');
+        elem.rel = 'stylesheet';
+        elem.href = url;
+        document.head.appendChild(elem);
+    }
+    function mostrar(argument) {
+        console.log(argument);
+    }
     loadCSS('https://fonts.googleapis.com/css?family=Roboto');
+
+    var $tvShowsContainer = $('.tv-shows');
+
+    function renderShows(shows) {
+        mostrar($tvShowsContainer);
+        $tvShowsContainer.hide().find('.loader').remove();
+        //mostrar(shows);
+        shows.forEach(function (show) {
+            var article = template
+                .replace(':name:', show.name)
+                .replace(':img:', (show.image == null) ? 'http://placehold.it/210x295/000000/ffffff' : show.image.medium)
+                .replace(':summary:', show.summary)
+                .replace(':imgalt:', show.name + ' Logo');
+            $tvShowsContainer.append($(article));
+        });
+        $tvShowsContainer.show('slow');
+    }
     //loadCSS('https://fonts.googleapis.com/css?family=Pacifico');
     /*
     var a = $('<a>', {
@@ -23,7 +49,18 @@ $(function () {
             var $busqueda = $(this)
                 .find('input[type="text"]')
                 .val();
-            alert('Se ha buscado: ' + $busqueda);
+            //alert('Se ha buscado: ' + $busqueda);
+            $.ajax({
+                url: 'http://api.tvmaze.com/search/shows',
+                data: { q: $busqueda },
+                success: function (res, textStatus, xhr) {
+                    var shows = res.map(function (el) {
+                        return el.show;
+                    });
+                    $tvShowsContainer.find('.tv-show').remove();
+                    renderShows(shows);
+                }
+            });
     });
 
     var template = '<article class="tv-show">' +
@@ -36,35 +73,21 @@ $(function () {
                     '</div>' +
                 '</article>';
 
-    $.ajax({
-        url: 'http://api.tvmaze.com/shows',
-        success: function (shows, textStatus, xhr) {
-            var $tvShowsContainer = $('.tv-shows');
-            shows.forEach(function (show) {
-                var article = template
-                    .replace(':name:', show.name)
-                    .replace(':img:', show.image.medium)
-                    .replace(':summary:', show.summary)
-                    .replace(':imgalt:', show.name + ' Logo');
-
-                $tvShowsContainer.append($(article));
-            })
-        }
-    });
-
-
-
-
-
-
-
-    function loadCSS(url) {
-        var elem = document.createElement('link');
-        elem.rel = 'stylesheet';
-        elem.href = url;
-        document.head.appendChild(elem);
+    if (!localStorage.shows) {
+        $.ajax('http://api.tvmaze.com/shows')
+            .then(function (shows) {
+                localStorage.shows = JSON.stringify(shows);
+                renderShows(shows);
+            });
+    } else {
+        renderShows(JSON.parse(localStorage.shows));
     }
-    function mostrar(argument) {
-        console.log(argument);
-    }
+
+
+
+
+
+
+
+
 })
